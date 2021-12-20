9eea167200239433630e7d14e752000ec8139ccc
erwann-dev <erwann.dev@gmail.com>
2021-12-20 04:33:51 +0100

    Work on contact part

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 50,
 });
+
+var clearButton = document.getElementById('discord');
+clearButton.onclick = discord_function;
+
+function discord_function(){
+	window.open("https://discordapp.com/users/188911626479599626/", "_blank",)
+}
+
+var clearButton = document.getElementById('mail');
+clearButton.onclick = mail_function;
+
+function mail_function(){
+	window.open("mailto:erwann.dev@gmail.com", "_blank",)
+}
